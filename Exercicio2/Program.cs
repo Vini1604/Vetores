818b378db2d801d3dbfd448967e1b8d6bb193ec0
Vini1604 <vinicius.oliveira1998@hotmail.com>
2022-06-02 18:29:49 -0300

    Resolvido o exercicio 2

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static ValorExtremo EncontraMaior(int[] vetorInteiros)
+        {
+            int maiorNumero = vetorInteiros[0], indiceMaior = 0;
+            for (int i = 0; i < vetorInteiros.Length; i++)
+            {
+                if (vetorInteiros[i] > maiorNumero)
+                {
+                    maiorNumero = vetorInteiros[i];
+                    indiceMaior = i;
+                }
+            }
+            return new ValorExtremo() { IndiceDoNumero = indiceMaior, ValorDoNumero = maiorNumero };
+
+        }
+
+        static ValorExtremo EncontraMenor(int[] vetorInteiros)
+        {
+            int menorNumero = vetorInteiros[0], indiceMenor = 0;
+            for (int i = 0; i < vetorInteiros.Length; i++)
+            {
+                if (vetorInteiros[i] < menorNumero)
+                {
+                    menorNumero = vetorInteiros[i];
+                    indiceMenor = i;
+                }
+            }
+            return new ValorExtremo() { IndiceDoNumero = indiceMenor, ValorDoNumero = menorNumero };
+
+        }
+
+        static void Main(string[] args)
+        {
+            int[] vetorInteiros = new int[20];
+            ValorExtremo maior, menor;
+
+            for (int i = 0; i < vetorInteiros.Length; i++)
+            {
+                Console.Write($"Insira um numero na posicao {i} do vetor: ");
+                vetorInteiros[i] = int.Parse(Console.ReadLine());
+            }
+            menor = EncontraMenor(vetorInteiros);
+            maior = EncontraMaior(vetorInteiros);
+            Console.WriteLine($"Menor -> {menor}");
+            Console.WriteLine($"Maior -> {maior}");
+            
+        }
+    }
+}
---
+++ Exercicio2/ValorExtremo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio2
+{
+    class ValorExtremo
+    {
+        public int ValorDoNumero { get; set; }
+        public int IndiceDoNumero { get; set; }
+        public override string ToString()
+        {
+            return $"Valor = {ValorDoNumero}, Indice = {IndiceDoNumero}";
+        }
+    }
+}
